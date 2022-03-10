14767760ad525c3a433184c272435d7883f613d6
Nathan <nbilleau@gmail.com>
2022-03-10 11:55:52 +0100

    home page

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,78 @@
-const form = document.querySelector('#loginForm')
+const form = document.querySelector('#registerForm')
+const cityInput = document.querySelector('input[name="city"]')
+const zipInput = document.querySelector('input[name="zip"]')
+const zipList = document.querySelector('#zipList')
+
+const citiesAndZip = [
+    {
+        city: 'Paris',
+        zip: ['75000', '75001', '75002', '75003', '75004', '75005', '75006', '75007', '75008', '75009', '75010', '75011', '75012', '75013', '75014', '75015', '75016', '75017', '75018', '75019', '75020']
+    },
+    {
+        city: 'Laval',
+        zip: ['53000']
+    },
+    {
+        city: 'Lyon',
+        zip: ['69000']
+    },
+    {
+        city: 'Le Mans',
+        zip: ['72000']
+    },
+    {
+        city: 'Bordeaux',
+        zip: ['33000']
+    },
+    {
+        city: 'Toulouse',
+        zip: ['31000']
+    }
+]
+
+const searchCityForZip = (e: Event) => {
+    // @ts-ignore
+    const zip = e.target.value
+
+    const zipFound = citiesAndZip.find(c => {
+        return c.zip.find(z => {
+            return z.startsWith(zip)
+        })
+    })
+
+    if (zipFound) {
+        // @ts-ignore
+        cityInput.value = zipFound.city
+
+        zipFound.zip.forEach(zip => {
+            const option = document.createElement('option')
+            option.value = zip
+            option.textContent = zip
+            zipList.appendChild(option)
+        })
+    }
+}
+
+
+const searchZipForCity = (e: Event) => {
+    // @ts-ignore
+    const city = e.target.value
+
+    const cityFound = citiesAndZip.find(c => {
+        return c.city.toLocaleLowerCase().includes(city.toLowerCase())
+    })
+
+ 
+    zipList.innerHTML = ''
+    if (cityFound) {
+        cityFound.zip.forEach(zip => {
+            const option = document.createElement('option')
+            option.value = zip
+            option.textContent = zip
+            zipList.appendChild(option)
+        })
+    }
+}
 
 const login = (e: Event) => {
     e.preventDefault()
@@ -10,10 +84,14 @@ const login = (e: Event) => {
             throw new Error('Le mot de passe ne correspond pas')
         if (!formData.get('password').toString().match(/\d+/g) )
             throw new Error('Le mot de passe doit contenir un chiffre')
+
+        window.location.href = '/'
     }
     catch (err) {
         alert(err)
     }
 }
 
+zipInput.addEventListener('keyup', searchCityForZip)
+cityInput.addEventListener('keyup', searchZipForCity)
 form.addEventListener('submit', login)
